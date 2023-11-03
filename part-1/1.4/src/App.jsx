4a86f0a232da5c14f233a9a3935bcb9b13242846
Liam <liamf.pro+koesio@proton.me>
2023-11-03 15:48:23 +0100

    [Exercise] 1.6

--- part-1/1.4/src/App.jsx
+++ part-1/1.4/src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 // Header Component
 const Header = ({ course }) => {
-
   console.log(course)
-
   return (
-    <h1>{course.name}</h1>
+    <h1>{course.name_test}</h1>
   )
 }
 
@@ -14,10 +12,6 @@ const Header = ({ course }) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-
       {parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)}
     </div>
   )
@@ -41,29 +35,27 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
   }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
---
+++ part-1/1.6/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
+const Header = ({ text }) => <h1>{text}</h1>
+
+// Content Component
+const Content = ({ datas }) => {
+  return (
+    <div>
+      {/* {datas.map(button => <Button onSmash={() => setRight(right + 1)} text={button.label} />)} */}
+      <Button onSmash={() => setGood(good + 1)} text={datas[0].label} />
+      <Button onSmash={() => setNeutral(neutral + 1)} text={datas[1].label} />
+      <Button onSmash={() => setBad(bad + 1)} text={datas[2].label} />
+    </div>
+  )
+}
+
+// Each individual Part within the Content
+const Part = ({ datas }) => {
+  return (
+    <div>
+      {datas.map(button => <p>{button.label} : {button.value}</p>)}
+    </div>
+  )
+}
+
+const Average = ({ datas }) => {
+  return (
+    <div>
+      {datas.map(button => <p>{button.label} : {button.value}</p>)}
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const data = [
+    {
+      label: "good",
+      value: good
+    },
+    {
+      label: "neutral",
+      value: neutral
+    },
+    {
+      label: "bad",
+      value: bad
+    },
+  ]
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button onSmash={() => setGood(good + 1)} text={data[0].label} />
+      <Button onSmash={() => setNeutral(neutral + 1)} text={data[1].label} />
+      <Button onSmash={() => setBad(bad + 1)} text={data[2].label} />
+      <Header text="statistics" />
+      <Part datas={data} />
+    </div>
+  )
+}
+
+export default App;
