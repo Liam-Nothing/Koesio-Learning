d2f1cc0958ce26da3330e8f62c7d379ac15d6c16
Liam <liamf.pro+koesio@proton.me>
2023-11-06 16:39:55 +0100

    [Exercise] 2.2

--- part-2/2.1/src/components/Course.jsx
+++ part-2/2.1/src/components/Course.jsx
@@ -8,6 +8,13 @@ const Part = ({ name, exercises }) => {
     )
 }
 
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return (
+      <p><b>Number of exercises {total}</b></p>
+    )
+}
+
 const Course = ({ course }) => {
 
     console.log(course);
@@ -18,6 +25,7 @@ const Course = ({ course }) => {
         <div>
             <Header text={course.name} />
             {parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)}
+            <Total parts={parts} />
         </div>
     )
   }
