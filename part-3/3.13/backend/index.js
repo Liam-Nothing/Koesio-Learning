c5afa1144e98f95376ea4475eb4b1925bccaae36
Liam <liamf.pro+koesio@proton.me>
2023-11-27 10:08:49 +0100

    [Exercise] 3.15

--- part-3/3.13/backend/index.js
+++ part-3/3.13/backend/index.js
@@ -9,8 +9,9 @@ const app = express()
 
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
+app.use(cors())
+// app.use(requestLogger)
 
 const mongoose = require('mongoose')
 
@@ -98,21 +99,34 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(
+            error => next(error)
+        )
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    // const id = Number(request.params.id)
-    const id = request.params.id
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = request.params.id
+//     persons = persons.filter(person => person.id !== id)
+//     response.status(204).end()
+// })
 
-    persons = persons.filter(person => person.id !== id)
-
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -132,3 +146,39 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
