a18ff6bc9386604a1e9d93e18f9fcd0246f04058
Liam <liamf.pro+koesio@proton.me>
2023-11-27 15:42:00 +0100

    [Exercise] 3.22

--- part-3/3.13/backend/index.js
+++ part-3/3.13/backend/index.js
@@ -3,24 +3,13 @@ require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
-const { v4: uuidv4 } = require('uuid');
-// const Note = require('./models/person')
 const app = express()
-
-
-
-
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-
-
-
 const mongoose = require('mongoose')
-
-const password = process.argv[2]
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
@@ -28,7 +17,7 @@ console.log('connecting to', url)
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB')
     })
     .catch((error) => {
@@ -46,7 +35,7 @@ const personSchema = new mongoose.Schema({
         minLength: 8,
         validate: {
             validator: function (num) {
-                return /^\d{2,3}-\d{5,}$/.test(num) && num.length >= 8;
+                return /^\d{2,3}-\d{5,}$/.test(num) && num.length >= 8
             },
             message: props => `${props.value} is not a valid phone number!`
         },
@@ -67,46 +56,21 @@ const Person = mongoose.model('Person', personSchema)
 module.exports = mongoose.model('Person', personSchema)
 
 const requestLogger = (req, res, next) => {
-    console.log('Method:', req.method);
-    console.log('Path:  ', req.path);
-    console.log('Body:  ', req.body);
-    console.log('---');
-    next();
-};
+    console.log('Method:', req.method)
+    console.log('Path:  ', req.path)
+    console.log('Body:  ', req.body)
+    console.log('---')
+    next()
+}
 
 app.use(requestLogger)
 
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
 
-let persons = [
-    {
-        "id": uuidv4(),
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": uuidv4(),
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": uuidv4(),
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": uuidv4(),
-        "name": "Mary Poppendieck",
-        "number": "39-2323-6423122"
-    }
-]
-
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+    console.log(`Server running on port ${PORT}`)
+})
 
 app.get('/info', (request, response) => {
     Person.find({}).then(persons => {
@@ -134,26 +98,18 @@ app.get('/api/persons/:id', (request, response, next) => {
         })
         .catch(
             error => {
-                // console.log(request.params.id)
                 next(error)
             }
         )
 })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = request.params.id
-//     persons = persons.filter(person => person.id !== id)
-//     response.status(204).end()
-// })
-
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
-        .then(result => {
+        .then(() => {
             response.status(204).end()
         })
         .catch(error => next(error))
 })
-
 
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
@@ -163,10 +119,6 @@ app.post('/api/persons', (request, response, next) => {
             error: 'content missing'
         })
     }
-
-    // if (body.content === undefined) {
-    //     return response.status(400).json({ error: 'content missing' })
-    // }
 
     const person = new Person({
         name: body.name,
@@ -179,7 +131,6 @@ app.post('/api/persons', (request, response, next) => {
         })
         .catch(error => next(error))
 })
-
 
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
@@ -198,7 +149,6 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
-
 const unknownEndpoint = (request, response) => {
     response.status(404).send(
         {
@@ -210,16 +160,17 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+    console.error(error.message)
 
     if (error.name === 'CastError') {
-        response.status(400).send({ error: 'malformatted id' });
+        response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
-        response.status(400).json({ error: error.message });
+        response.status(400).json({ error: error.message })
     } else {
-        response.status(500).json({ error: 'Internal server error' });
+        response.status(500).json({ error: 'Internal server error' })
     }
-}
 
+    next(error)
+}
 
 app.use(errorHandler)
