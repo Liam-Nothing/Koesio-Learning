a3a75b1857de4844c85bd46c974bcc6f4e1d46da
Liam <liamf.pro+koesio@proton.me>
2023-11-27 15:17:40 +0100

    [Exercise] 3.20

--- part-3/3.13/backend/index.js
+++ part-3/3.13/backend/index.js
@@ -41,7 +41,17 @@ const personSchema = new mongoose.Schema({
         minLength: 5,
         required: true
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (num) {
+                return /^\d{2,3}-\d{5,}$/.test(num) && num.length >= 8;
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 })
 
 personSchema.set('toJSON', {
