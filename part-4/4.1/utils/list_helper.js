46d92edcf6ddb165ce8b0c19750fc7cac381bd60
Liam <liamf.pro+koesio@proton.me>
2023-11-28 16:48:59 +0100

    [Exercise] 4.4

---
+++ part-4/4.1/utils/list_helper.js
@@ -0,0 +1,18 @@
+// Example helper function
+const dummy = () => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
