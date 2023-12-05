e6ec960e85f39ab1688d0ac2b7e50bc19bc32966
Liam <liamf.pro+koesio@proton.me>
2023-12-05 10:25:09 +0100

    [Exercise] 4.20

--- part-4/4.1/app.js
+++ part-4/4.1/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const requestLogger = require('./utils/requestLogger')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
---
+++ part-4/4.1/utils/middleware.js
@@ -0,0 +1,13 @@
+const tokenExtractor = (request, res, next) => {
+
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        request.token = authorization.replace('Bearer ', '')
+    }
+
+    next()
+}
+
+module.exports = {
+    tokenExtractor
+}
